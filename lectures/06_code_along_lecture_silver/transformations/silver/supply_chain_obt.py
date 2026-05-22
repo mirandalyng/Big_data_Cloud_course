bd0e1bdc9a3a0cd68b84e687d14ff0d9caa284cc
mirandalyng <lyngmiranda@gmail.com>
2026-05-22 08:53:56 +0000

    finishing silver layer - gold layer - fct table

--- lectures/06_code_along_lecture_silver/transformations/silver/supply_chain_obt.py
+++ lectures/06_code_along_lecture_silver/transformations/silver/supply_chain_obt.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import to_timestamp, col, coalesce, lit, when
+from pyspark.sql.functions import to_timestamp, col, coalesce, lit, when, round as spark_round
 from pyspark import pipelines as dp
 from utils.utils import rename_columns_to_snake_case
 
@@ -28,12 +28,28 @@ def cleaned_supply_chain():
             coalesce(col("customer_zipcode").cast("string"), lit("unknown")),
         )
         .withColumn(
+        "customer_lname", coalesce(col("customer_lname"), lit("unknown"))
+        )
+        .withColumn(
             "customer_country",
             when(col("customer_country") == "EE. UU.", "United States").otherwise(
                 col("customer_country")
             ),
         )
         .withColumn("order_date", to_timestamp("order_date_(dateorders)", "M/d/yyyy H:mm"))
+        
+        .withColumn(
+        "order_item_product_price", spark_round(col("order_item_product_price"), 2))
+        .withColumn("benefit_per_order", spark_round(col("benefit_per_order"), 2))
+        .withColumn("sales_per_customer", spark_round(col("sales_per_customer"), 2))
+        .withColumn(
+        "order_item_total",
+        spark_round(
+            col("order_item_quantity") 
+            * col("order_item_product_price") 
+            * (1 - col("order_item_discount_rate")), 
+            2))
+        
     ).drop(
         "customer_email",
         "customer_password",
