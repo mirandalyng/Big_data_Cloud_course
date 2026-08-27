52092e6516dbca4a8f6e89727ffe8cc2ae87b3a7
Miranda Lyng <lyngmiranda@gmail.com>
2026-08-27 10:51:28 +0200

    create frontend image

--- lectures/15_azure_fastAPI/frontend/src/frontend/dashboard.py
+++ lectures/15_azure_fastAPI/frontend/src/frontend/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import httpx
 import os
 
+# try to get enviroment variale BACKEND_IRL , if not exiddt default to 2nd argument
 BASE_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 
@@ -10,6 +11,8 @@ BASE_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 def main():
     st.markdown("# PokeDash")
+
+    st.write(BASE_URL)
 
     stats = httpx.get(f"{BASE_URL}/pokemons/stats",
                       timeout=30).json()
